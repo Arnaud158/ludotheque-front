022bc44bab06dd07d5fd22ca4ac8fea3c8550cec
Elsa <elsa.logier@gmail.com>
2023-04-12 11:04:54 +0200

    génération de carte-jeu composant

---
+++ src/app/carte-jeu/carte-jeu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carte-jeu',
+  templateUrl: './carte-jeu.component.html',
+  styleUrls: ['./carte-jeu.component.css']
+})
+export class CarteJeuComponent {
+
+}
