e243b22852ee525c82bdd76cde81de3492f3941d
Santoro Thomas <thomas_santoro@ens.univ-artois.fr>
2023-04-12 13:48:28 +0200

    fix #7 PA01 sauf avatar et route a ajouter au fur et à mesure

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,17 @@
 export interface User {
-    id: number,
-    name: string,
+    avatar: number,
+    created_at: string,
     email: string,
-    jwtToken?: string
+    id: string,
+    name: number,
+    nom: string,
+    prenom: string,
+    pseudo: string,
+    updated_at: string,
+    valide: string,
+    jwtToken?: String
 }
 
-export const ANONYMOUS_USER = {id:-1,name:"anonymous_user",email:"anonymous@example.com"}
+//export const ANONYMOUS_USER = {id:-1,name:"anonymous_user",email:"anonymous@example.com"}
+
+export const ANONYMOUS_USER : User = <User>{};
