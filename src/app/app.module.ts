73d4b55994b19399f301ee64551dcc0a60122c7f
Elsa <elsa.logier@gmail.com>
2023-04-12 08:33:11 +0200

    pied de page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PiedPageComponent } from './pied-page/pied-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PiedPageComponent
   ],
   imports: [
     BrowserModule
