d6b83b3d546e902e3004d5767b3596216772e083
Jules B <bobeuf.jules@gmail.com>
2023-04-13 12:34:22 +0200

    debug accueil, pull, push

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
 import {JeuService} from "../jeu.service";
-import {JeuRequest} from "../../models/jeu-request";
-import {DataSourceAsynchro} from "./dataSourceAsynchro";
 
 @Component({
   selector: 'app-accueil',
@@ -9,13 +7,8 @@ import {DataSourceAsynchro} from "./dataSourceAsynchro";
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent {
-  public listeJeu : JeuRequest[] = []
-  dataSource: DataSourceAsynchro = new DataSourceAsynchro(this.jeuService);
-  lesColonnes = ['nom', 'description', 'langue', 'categorie', 'theme'];
 
   constructor(public jeuService : JeuService) {
-    this.dataSource.setData();
-    console.log(this.dataSource)
   }
 }
 
